ece1122d0b55b02569546af5d50d7dd088bba6bf
Rune V. Sjøen <rvsjoen@gmail.com>
2011-06-27 16:16:57 +0200

    Started rewrite to phing, so far so good

---
+++ 17/tpl_helloworld/index.php
@@ -0,0 +1,10 @@
+<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+	<head>
+		<jdoc:include type="head" />
+	</head>
+	<body>
+		<jdoc:include type="component" />
+	</body>
+</html>
