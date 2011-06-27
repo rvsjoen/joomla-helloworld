f69911af193c7281f7d5ac171cf06994f0a52c77
Rune V. Sjøen <rvsjoen@gmail.com>
2011-06-27 13:52:26 +0200

    Some cleanup

--- 17/mod_helloworld/mod_helloworld.php
+++ 17/mod_helloworld/mod_helloworld.php
@@ -1,1 +1,10 @@
-<p>Hello World!</p>
+<?php defined('_JEXEC') or die;
+/**
+ * @version		$Id$
+ * @package		Helloworld
+ * @subpackage	mod_helloworld
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+require_once dirname(__FILE__).'/helper.php';
