31545cd2a32f9dac6edb2390651c33494d7c166d
Rune V. Sjøen <rvsjoen@gmail.com>
2011-06-27 17:49:50 +0200

    Brush up template

--- 17/tpl_helloworld/component.php
+++ 17/tpl_helloworld/component.php
@@ -1,1 +1,11 @@
+<?php defined('_JEXEC') or die;
+/**
+ * @version		$Id$
+ * @package		HelloWorld
+ * @subpackage	tpl_helloworld
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+?>
 <jdoc:include type="component" />
--- 17/tpl_helloworld/index.php
+++ 17/tpl_helloworld/index.php
@@ -1,10 +1,22 @@
-<?php defined( '_JEXEC' ) or die( 'Restricted access' ); ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+<?php defined('_JEXEC') or die;
+/**
+ * @version		$Id$
+ * @package		HelloWorld
+ * @subpackage	tpl_helloworld
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+?>
+<?php echo '<?'; ?>xml version="1.0" encoding="<?php echo $this->_charset ?>"?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>" >
 	<head>
 		<jdoc:include type="head" />
 	</head>
 	<body>
+		<jdoc:include type="modules" name="top" style="none" />
+		<jdoc:include type="message" />
 		<jdoc:include type="component" />
 	</body>
 </html>
