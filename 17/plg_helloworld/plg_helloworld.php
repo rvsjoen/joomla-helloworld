507b7443066f39fcaf20f29b97bdaaf716d18058
Rune V. Sjøen <rvsjoen@gmail.com>
2011-06-27 18:01:59 +0200

    Update plugin to 1.7

--- 17/plg_helloworld/plg_helloworld.php
+++ 17/plg_helloworld/plg_helloworld.php
@@ -1,16 +1,22 @@
-<?php defined('_JEXEC') or die(); 
+<?php defined('_JEXEC') or die;
+/**
+ * @version		$Id$
+ * @package		HelloWorld
+ * @subpackage	plg_helloworld
+ * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ */
 
 jimport('joomla.event.plugin');
 
 class plgSystemHelloWorld extends JPlugin {
-	
-	function onAfterInitialise(){
-		// TODO Action here
+
+	public function __construct(& $subject, $config){
+		parent::__construct($subject, $config);
+		$this->loadLanguage();
 	}
-	
-	function onAfterRoute(){
-		// TODO Action here
+
+	function onAfterInitialise(){
+		//TODO Do work here
 	}
 }
-
-?>
