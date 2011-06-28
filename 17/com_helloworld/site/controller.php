3dc5d7e166ffdeac5c68f85bb9025883d67d933c
Rune V. Sjøen <rvsjoen@gmail.com>
2011-06-28 15:00:12 +0200

    Fix file header blocks

--- 17/com_helloworld/site/controller.php
+++ 17/com_helloworld/site/controller.php
@@ -2,9 +2,9 @@
 /**
  * @version		$Id$
  * @package		HelloWorld
- * @subpackage	com_helloworld
+ * @subpackage	Component
  * @copyright	Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @license		http://www.gnu.org/licenses/gpl-2.0.html
  */
 
 jimport('joomla.application.component.controller');
