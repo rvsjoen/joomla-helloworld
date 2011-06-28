98ee219cd183996c10bd2d530a0ed1908c94a014
Rune V. Sjøen <rvsjoen@gmail.com>
2011-06-28 14:44:13 +0200

    Make index.html files valid

--- 17/com_helloworld/admin/sql/uninstall.mysql.utf8.sql
+++ 17/com_helloworld/admin/sql/uninstall.mysql.utf8.sql
@@ -1,1 +1,7 @@
+-- @version             $Id$
+-- @package             HelloWorld
+-- @subpackage  		Component
+-- @copyright   		Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+-- @license             GNU General Public License version 2 or later; see LICENSE.txt
+
 SELECT 1;
